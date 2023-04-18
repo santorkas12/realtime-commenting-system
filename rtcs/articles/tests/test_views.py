4727f1750c5b838bf324fe20f192c71923fef715
Stelios Antorkas <72335516+santorkas12@users.noreply.github.com>
2023-04-18 23:49:07 +0300

    RTCS-15 Update articles/views unit tests

--- rtcs/articles/tests/test_views.py
+++ rtcs/articles/tests/test_views.py
@@ -1,4 +1,3 @@
-import base64
 import json
 from django.contrib.auth import get_user_model
 from django.test import TestCase
@@ -18,7 +17,9 @@ class TestArticleService(TestCase):
             "title": "Article from test API",
             "content": "Article from test API - Content"
         }
+
         cls.post_response = c.post('/articles/', json.dumps(cls.post_payload), content_type='application/json')
+        cls.article_id = cls.post_response.json().get('id')
         return super().setUpClass()
     
     def test_create_article(cls):
@@ -34,10 +35,9 @@ class TestArticleService(TestCase):
 
         cls.assertEqual(get_response.status_code, 200)
         cls.assertIsInstance(get_response_data, list)
-        cls.assertEqual(len(get_response_data), 1)
 
     def test_retrieve_article(cls):
-        retrieve_response = c.get('/articles/1')
+        retrieve_response = c.get(f'/articles/{cls.article_id}')
         retrieve_response_data = retrieve_response.json()
 
         cls.assertEqual(retrieve_response.status_code, 200)
@@ -49,7 +49,8 @@ class TestArticleService(TestCase):
             "title": "Article from test API - PUT Modified",
             "content": "Article from test API - Content - PUT Modified"
         }
-        put_response = c.put('/articles/1', json.dumps(put_payload), content_type='application/json')
+
+        put_response = c.put(f'/articles/{cls.article_id}', json.dumps(put_payload), content_type='application/json')
         put_response_data = put_response.json()
 
         cls.assertEqual(put_response.status_code, 200)
@@ -57,13 +58,11 @@ class TestArticleService(TestCase):
         cls.assertEqual(put_response_data.get('content'), put_payload.get('content'))
         
     def test_delete_article(cls):
-        delete_response = c.delete('/articles/1')
-
+        delete_response = c.delete(f'/articles/{cls.article_id}')
         cls.assertEqual(delete_response.status_code, 204)
-
-        get_response = c.get('/articles/')
-        cls.assertEqual(get_response.status_code, 200)
-        cls.assertFalse(get_response.json())
+        
+        retrieve_deleted_article_response = c.get(f'/articles/{cls.article_id}')
+        cls.assertEqual(retrieve_deleted_article_response.status_code, 404)
         
 
 class TestCommentService(TestCase):
@@ -73,49 +72,49 @@ class TestCommentService(TestCase):
         user = get_user_model()
         cls.comment_service_request_user = user.objects.create(username='comment_service', password='123456', is_superuser=True)
         c.force_login(cls.comment_service_request_user)
-
         
         create_article_payload = {
             "title": "Article from test API",
             "content": "Article from test API - Content"
         }
-        c.post('/articles/', json.dumps(create_article_payload), content_type='application/json')
 
+        post_articles_response = c.post('/articles/', json.dumps(create_article_payload), content_type='application/json')
+        cls.article_id = post_articles_response.json().get('id')
         cls.post_payload = {
-            "content": "This is the 1st comment on article 1 in test db. It only needs a 'content' payload key. All other attributes are handled by the server!"
+            "content": "This is a comment on an article in test db. It only needs a 'content' payload key. All other attributes are handled by the server!"
         }
-        cls.post_response = c.post('/articles/1/comments', json.dumps(cls.post_payload), content_type='application/json')
+        cls.post_comments_response = c.post(f'/articles/{cls.article_id}/comments', json.dumps(cls.post_payload), content_type='application/json')
+        cls.comment_id = cls.post_comments_response.json().get('id')
         return super().setUpClass()
     
     def test_create_comment(cls):
-        post_response_data = cls.post_response.json()
+        post_response_data = cls.post_comments_response.json()
 
-        cls.assertEqual(cls.post_response.status_code, 201)
-        cls.assertEqual(post_response_data.get('article'), 1)
+        cls.assertEqual(cls.post_comments_response.status_code, 201)
+        cls.assertEqual(post_response_data.get('article'), cls.article_id)
         cls.assertEqual(post_response_data.get('content'), cls.post_payload.get('content'))
         cls.assertEqual(post_response_data.get('author'), cls.comment_service_request_user.id)
 
     def test_get_article_comments(cls):
-        get_response = c.get('/articles/1/comments')
+        get_response = c.get(f'/articles/{cls.article_id}/comments')
         get_response_data = get_response.json()
 
         cls.assertEqual(get_response.status_code, 200)
         cls.assertIsInstance(get_response_data, list)
-        cls.assertEqual(len(get_response_data), 1)
 
     def test_put_article_comment(cls):
         put_payload = {
             "content": "This is the 1st comment on article 1 in test db. Its content has been modified by a put request."
         }
-        put_response = c.put('/articles/1/comments/1', json.dumps(put_payload), content_type='application/json')
-        put_response_data = put_response.json()
 
+        put_response = c.put(f'/articles/{cls.article_id}/comments/{cls.comment_id}', json.dumps(put_payload), content_type='application/json')
+        put_response_data = put_response.json()
         cls.assertEqual(put_response.status_code, 200)
         cls.assertEqual(put_response_data.get('content'), put_payload.get('content'))
 
     def test_delete_article_comment(cls):
-        delete_response = c.delete('/articles/1/comments/1')
+        delete_response = c.delete(f'/articles/{cls.article_id}/comments/{cls.comment_id}')
 
         cls.assertEqual(delete_response.status_code, 204)
-        get_response = c.get('/articles/1/comments/1')
+        get_response = c.get(f'/articles/{cls.article_id}/comments/{cls.comment_id}')
         cls.assertEqual(get_response.status_code, 404)
